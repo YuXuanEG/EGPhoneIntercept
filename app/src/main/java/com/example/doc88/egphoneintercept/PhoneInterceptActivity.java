2d9dc726c0c6b33362a008da0e9ccd7d56c0a8ed
古德祯 <gudz@shengruan.com>
2018-02-09 18:03:33 +0800

    添加跳转权限设置

--- app/src/main/java/com/example/doc88/egphoneintercept/PhoneInterceptActivity.java
+++ app/src/main/java/com/example/doc88/egphoneintercept/PhoneInterceptActivity.java
@@ -164,7 +164,7 @@ public class PhoneInterceptActivity extends AppCompatActivity {
                             .setPositiveButton("去设置", new DialogInterface.OnClickListener() {
                                 @Override
                                 public void onClick(DialogInterface dialog, int which) {
-
+                                    getAppDetailSettingIntent(PhoneInterceptActivity.this);
                                     dialog.dismiss();
                                 }
                             })
@@ -183,6 +183,46 @@ public class PhoneInterceptActivity extends AppCompatActivity {
                 super.onRequestPermissionsResult(requestCode, permissions,
                         grantResults);
         }
+    }
+
+
+    /**
+     * 跳转到权限设置界面
+     不知道软件管家包名
+     eg：打开vivo的 i管家，在Terminal中通过执行这个指令 adb shell dumpsys activity top来捕捉软件的主Activity。
+     adb结果：
+     vivo的软件管家包名：com.iqoo.secure
+     oppo的软件管家包名：com.oppo.safe
+     */
+    private void getAppDetailSettingIntent(Context context){
+
+        // vivo 点击设置图标>加速白名单>我的app
+        //      点击软件管理>软件管理权限>软件>我的app>信任该软件
+        Intent appIntent = context.getPackageManager().getLaunchIntentForPackage("com.iqoo.secure");
+        if(appIntent != null){
+            context.startActivity(appIntent);
+            return;
+        }
+
+        // oppo 点击设置图标>应用权限管理>按应用程序管理>我的app>我信任该应用
+        //      点击权限隐私>自启动管理>我的app
+        appIntent = context.getPackageManager().getLaunchIntentForPackage("com.oppo.safe");
+        if(appIntent != null){
+            context.startActivity(appIntent);
+            return;
+        }
+
+        Intent intent = new Intent();
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        if(Build.VERSION.SDK_INT >= 9){
+            intent.setAction("android.settings.APPLICATION_DETAILS_SETTINGS");
+            intent.setData(Uri.fromParts("package", getPackageName(), null));
+        } else if(Build.VERSION.SDK_INT <= 8){
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setClassName("com.android.settings","com.android.settings.InstalledAppDetails");
+            intent.putExtra("com.android.settings.ApplicationPkgName", getPackageName());
+        }
+        startActivity(intent);
     }
 
     @Override
